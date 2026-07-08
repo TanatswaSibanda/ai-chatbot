7ec49bc1a89546bb6da83de3b7ae23f6d5db1d40
TanatswaSibanda <tanatswa.m.sibanda@gmail.com>
2026-07-08 23:46:51 +0200

    Feature: Create Chat Input feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import dropdown from './assets/keyboard_arrow_up.png'
 import person from './assets/Logo.png'
+import add from './assets/Plus.png'
+import submit from './assets/arrow_forward.png'
 import './App.css'
 
 function App() {
 
   return (
-    <div>
+    <div className='container'>
       <div className='header'>
         <button type="button" className='model-selector' img src={dropdown} alt="image" >
           OpenAI GPT-5
@@ -15,7 +17,11 @@ function App() {
       <div className='bottom'>
         <img src={person} alt="" className='person-icon' />
         <h3>How can I help you today?</h3>
-
+        <div className='input' >
+          <img src={add} alt='' width={40} />
+          <input type="text" id="input" placeholder='Ask me anything...' />
+          <img src={submit} alt="" width={40} height={40} />
+        </div>
       </div>
     </div>
 
