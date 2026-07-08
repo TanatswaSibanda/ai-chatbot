2c0d1511573c9e811b0fa9b1853b806e5ea6e1af
TanatswaSibanda <tanatswa.m.sibanda@gmail.com>
2026-07-08 22:49:52 +0200

    Chore: Added Icon and Opening Text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import dropdown from './assets/keyboard_arrow_up.png'
+import person from './assets/Logo.png'
 import './App.css'
 
 function App() {
@@ -10,6 +11,11 @@ function App() {
           OpenAI GPT-5
           <img src={dropdown} alt="image" className='img' />
         </button>
+      </div>
+      <div className='bottom'>
+        <img src={person} alt="" className='person-icon' />
+        <h3>How can I help you today?</h3>
+
       </div>
     </div>
 
